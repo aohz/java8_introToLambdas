f93283400b2ab7217d7c52a5de77136530b9f8e6
AOHZ <AOHZ@gft.com>
2016-01-25 16:40:31 +0100

    Adding method reference exercise

---
+++ src/part4/methodreference/exercises/Sample1_methodRererence.java
@@ -0,0 +1,25 @@
+package part4.methodreference.exercises;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.LongSupplier;
+
+import part4.methodreference.model.Student;
+
+/**
+ * Replace the following lambda expressions by a equilaven Method reference call
+ * 
+ * @author aohz
+ *
+ */
+public class Sample1_methodRererence {
+	public static void main(String[] args) {
+
+		// Ex 1
+		Function<Student, Integer> getStudentAge = (s) -> s.getAge();
+		// Ex 2
+		LongSupplier getThreadId = () -> Thread.currentThread().getId();
+		// Ex 3
+		BiFunction<String, String, String> getSubString = (s1, s2) -> s1 + s2;
+	}
+}
---
+++ src/part4/methodreference/solution/Sample1_methodRererence.java
@@ -0,0 +1,32 @@
+package part4.methodreference.solution;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.LongSupplier;
+
+import part4.methodreference.model.Student;
+
+/**
+ * Replace the following lambda expressions by a equilaven Method reference call
+ * 
+ * @author aohz
+ *
+ */
+public class Sample1_methodRererence {
+	public static void main(String[] args) {
+
+		// Ex 1
+		Function<Student, Integer> getStudentAge = (s) -> s.getAge();				
+		// Ex 2
+		LongSupplier getThreadId = () -> Thread.currentThread().getId();
+		// Ex 3
+		BiFunction<String, String, String> getSubString = (s1, s2) -> s1 + s2;
+						
+		// Sol 1
+		Function<Student, Integer> getStudentAgeMR = Student::getAge;				
+		// Sol 2
+		LongSupplier getThreadIdMR = Thread.currentThread()::getId;
+		// Sol 3
+		BiFunction<String, String, String> getSubStringMR = String::concat;
+	}
+}
